af368bd949391470c3296d38ff240290f44a929f
ntobeko860 <102877211+ntobeko860@users.noreply.github.com>
2022-04-24 10:46:33 +0200

    Update assistant.cpp

--- assistant.cpp
+++ assistant.cpp
@@ -1,16 +1,4 @@
 
-/*
-----------------------------------------------------------------------------
-               Project title   : Jarvis(personal assistant)
-               Made By         : Vinay Hajare
-               Written in      : C++
-               Started project : 1 April , 2021
-               Platform        : Windows
-               IDE used        : Code blocks
-               Last Modified   : 1 April , 2022
-               Total Code      : 700+ Lines
- ----------------------------------------------------------------------------
-*/
 
 
 //---------------assistant.c++-----------------
